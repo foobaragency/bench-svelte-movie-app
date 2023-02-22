7c864b3642b88f08a498958d6c7e404957af0eed
Patrick Maloney <patrick@foobar.agency>
2023-02-22 16:59:29 +0100

    get scrolling working for categories

--- src/routes/Category.svelte
+++ src/routes/Category.svelte
@@ -4,20 +4,24 @@
 	const imageWidth = 185;
 </script>
 
-<div class="flex pb-2 [&>*]:mr-2 [&>*]:w-48 overflow-x-auto">
-	{#each movies as movie}
-		<figure class="mx-auto p-8 md:p-0">
-			<img
-				class="mx-auto rounded-xl shadow-md  shadow-slate-400 md:h-auto"
-				src="{IMAGE_BASE_URL}/w{imageWidth}/{movie.poster_path}"
-				alt={movie.title}
-			/>
-			<figcaption class="p-1.5 text-slate-900 dark:text-slate-700">
-				<div class="text-sm font-bold">{movie.title}</div>
-				<div class="text-xs font-thin">
-					{new Date(movie.release_date).toDateString()}
-				</div>
-			</figcaption>
-		</figure>
-	{/each}
+<div style="overflow: scroll hidden">
+	<div style="min-width: 100%; display: table;">
+		<div class="flex flex-nowrap pb-2 [&>*]:mr-2 [&>*]:w-48">
+			{#each movies as movie}
+				<figure class="mx-auto p-8 md:p-0">
+					<img
+						class="mx-auto rounded-xl shadow-md  shadow-slate-400 md:h-auto"
+						src="{IMAGE_BASE_URL}/w{imageWidth}/{movie.poster_path}"
+						alt={movie.title}
+					/>
+					<figcaption class="p-1.5 text-slate-900 dark:text-slate-700">
+						<div class="text-sm font-bold">{movie.title}</div>
+						<div class="text-xs font-thin">
+							{new Date(movie.release_date).toDateString()}
+						</div>
+					</figcaption>
+				</figure>
+			{/each}
+		</div>
+	</div>
 </div>
