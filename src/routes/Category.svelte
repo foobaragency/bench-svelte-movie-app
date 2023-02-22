a30a3201c0f9b108539616d98b96fcec057955cb
Patrick Maloney <patrick@foobar.agency>
2023-02-22 17:49:50 +0100

    add link to movie details page

--- src/routes/Category.svelte
+++ src/routes/Category.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import MovieCard from './MovieCard.svelte';
 
-	export let movies: { title: string; release_date: string; poster_path: string }[];
+	export let movies: { id: string; title: string; release_date: string; poster_path: string }[];
 </script>
 
 <div style="overflow: scroll hidden">
@@ -9,6 +9,7 @@
 		<div class="flex flex-nowrap pb-2 [&>*]:mr-2 [&>*]:w-48">
 			{#each movies as movie}
 				<MovieCard
+					id={movie.id}
 					title={movie.title}
 					releaseDate={movie.release_date}
 					poster={movie.poster_path}
--- src/routes/MovieCard.svelte
+++ src/routes/MovieCard.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
-	export let title: string, releaseDate: string, poster: string;
+	export let title: string, releaseDate: string, poster: string, id: string;
 
 	type ImageWidth = '185' | '342' | '500' | '780';
+
 	export let imageWidth: ImageWidth = '185';
 	const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
 </script>
 
-<figure class="mx-auto p-8 md:p-0">
-	<img
-		class="mx-auto rounded-xl shadow-md  shadow-slate-400 md:h-auto"
-		src="{IMAGE_BASE_URL}/w{imageWidth}/{poster}"
-		alt={title}
-	/>
-	<figcaption class="p-1.5 text-slate-900 dark:text-slate-700">
-		<div class="text-sm font-bold">{title}</div>
-		<div class="text-xs font-thin">
-			{new Date(releaseDate).toDateString()}
-		</div>
-	</figcaption>
-</figure>
+<a href="/movie/{id}">
+	<figure class="mx-auto p-8 md:p-0">
+		<img
+			class="mx-auto rounded-xl shadow-md  shadow-slate-400 md:h-auto"
+			src="{IMAGE_BASE_URL}/w{imageWidth}/{poster}"
+			alt={title}
+		/>
+		<figcaption class="p-1.5 text-slate-900 dark:text-slate-700">
+			<div class="text-sm font-bold">{title}</div>
+			<div class="text-xs font-thin">
+				{new Date(releaseDate).toDateString()}
+			</div>
+		</figcaption>
+	</figure>
+</a>
