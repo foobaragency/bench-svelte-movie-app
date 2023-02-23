fdfb8689e3dfc9ec5fbe2201736000441476ae72
Patrick Maloney <patrick@foobar.agency>
2023-02-23 10:40:55 +0100

    add some rough designs for movie details page

--- src/routes/movie/[slug]/+page.svelte
+++ src/routes/movie/[slug]/+page.svelte
@@ -1,13 +1,26 @@
 <script>
-	import MovieCard from '../../MovieCard.svelte';
-
 	/** @type {import('./$types').PageData} */
 	export let data;
+	console.log('data', data);
+	const { id, title, poster_path, backdrop_path, release_date, overview, genres } = data;
+	const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
 </script>
 
-<MovieCard
-	title={data.title}
-	poster={data.poster_path}
-	releaseDate={data.release_date}
-	imageWidth="500"
-/>
+<div class="bg-cover" style="background-image: url({IMAGE_BASE_URL}/original/{backdrop_path})">
+	<div
+		class="w-full h-full flex flex-row p-12 backdrop--sm from-gray-900 to-transparent bg-gradient-to-b"
+	>
+		<img src="{IMAGE_BASE_URL}/w500/{poster_path}" alt={title} class="rounded-xl shadow-xl mr-12" />
+		<div class="flex flex-col">
+			<h1 class="text-white text-6xl">{title}</h1>
+			<p class="text-white">
+				{release_date}
+				{#each genres as genre}
+					<p>{genre.name}</p>
+				{/each}
+			</p>
+			<h2 class="text-white font-medium">Overview</h2>
+			<p class="text-white">{overview}</p>
+		</div>
+	</div>
+</div>
