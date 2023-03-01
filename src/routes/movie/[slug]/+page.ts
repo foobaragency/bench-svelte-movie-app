16b75992b843aa63d716faca64cefa373beda44a
Patrick Maloney <patrick@foobar.agency>
2023-03-01 10:43:33 +0100

    remove popularity sorting for credits and related movies

--- src/routes/movie/[slug]/+page.ts
+++ src/routes/movie/[slug]/+page.ts
@@ -1,20 +1,24 @@
-import type { MovieDetails, MovieDisplay } from "../../../types/movie";
-import type { PageLoad } from "./$types";
+import type { MovieDetails, MovieDisplay } from '../../../types/movie';
+import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ params, fetch }): Promise<MovieDetails> => {
-  const movieDetails = await fetch(
-    `https://api.themoviedb.org/3/movie/${params.slug}?api_key=061b5b5397826fffc37bcaad1cc6814f`
-  );
+	const movieDetails = await fetch(
+		`https://api.themoviedb.org/3/movie/${params.slug}?api_key=061b5b5397826fffc37bcaad1cc6814f`
+	);
 
-  const similarMovies = await fetch(
-    `https://api.themoviedb.org/3/movie/${params.slug}/similar?api_key=061b5b5397826fffc37bcaad1cc6814f`
-  );
-  const similar: { results: (MovieDisplay & { popularity: number })[] } = await similarMovies.json();
+	const similarMovies = await fetch(
+		`https://api.themoviedb.org/3/movie/${params.slug}/similar?api_key=061b5b5397826fffc37bcaad1cc6814f`
+	);
+	const similar: { results: (MovieDisplay & { popularity: number })[] } =
+		await similarMovies.json();
 
-  const credits = await fetch(
-    `https://api.themoviedb.org/3/movie/${params.slug}/credits?api_key=061b5b5397826fffc37bcaad1cc6814f`
-  );
+	const credits = await fetch(
+		`https://api.themoviedb.org/3/movie/${params.slug}/credits?api_key=061b5b5397826fffc37bcaad1cc6814f`
+	);
 
-  return { movie: await movieDetails.json(), credits: await credits.json(), similar: similar.results.sort((a, b) => b.popularity - a.popularity) };
-
-}
+	return {
+		movie: await movieDetails.json(),
+		credits: await credits.json(),
+		similar: similar.results
+	};
+};
--- src/routes/person/[slug]/+page.ts
+++ src/routes/person/[slug]/+page.ts
@@ -1,18 +1,26 @@
-import type { MovieDisplay } from "@/types/movie";
-import type { PersonDetails } from "../../../types/person";
-import type { PageLoad } from "./$types";
+import type { MovieDisplay } from '@/types/movie';
+import type { PersonDetails } from '../../../types/person';
+import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ params, fetch }): Promise<{ details: PersonDetails, credits: MovieDisplay[] }> => {
-  const personDetails = await fetch(
-    `https://api.themoviedb.org/3/person/${params.slug}?api_key=061b5b5397826fffc37bcaad1cc6814f`
-  );
+export const load: PageLoad = async ({
+	params,
+	fetch
+}): Promise<{ details: PersonDetails; credits: MovieDisplay[] }> => {
+	const personDetails = await fetch(
+		`https://api.themoviedb.org/3/person/${params.slug}?api_key=061b5b5397826fffc37bcaad1cc6814f`
+	);
 
-  const combinedCredits = await fetch(
-    `https://api.themoviedb.org/3/person/${params.slug}/combined_credits?api_key=061b5b5397826fffc37bcaad1cc6814f`
-  );
-  const { cast, crew }: { cast: (MovieDisplay & { popularity: number })[], crew: (MovieDisplay & { popularity: number })[] } = await combinedCredits.json();
-  const allCredits = [...cast, ...crew].sort((a, b) => b.popularity - a.popularity);
+	const combinedCredits = await fetch(
+		`https://api.themoviedb.org/3/person/${params.slug}/combined_credits?api_key=061b5b5397826fffc37bcaad1cc6814f`
+	);
+	const {
+		cast,
+		crew
+	}: {
+		cast: (MovieDisplay & { popularity: number })[];
+		crew: (MovieDisplay & { popularity: number })[];
+	} = await combinedCredits.json();
+	const allCredits = [...cast, ...crew];
 
-  return { details: await personDetails.json(), credits: allCredits };
-
-}
+	return { details: await personDetails.json(), credits: allCredits };
+};
